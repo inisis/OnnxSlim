d2e7e208dc673a8f08457b818e06458cd285e24e
UltralyticsAssistant <web@ultralytics.com>
2024-10-29 04:18:53 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,34 +1,40 @@
 import os
-import pytest
-import tempfile
 import subprocess
+import tempfile
 
 import numpy as np
 import onnxruntime as ort
+import pytest
+
 ort.set_default_logger_severity(3)
 
 from onnxslim.utils import print_model_info_as_table, summarize_model
 
 MODELZOO_PATH = "/data/modelzoo"
 
+
 def bench_main(command):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result
+
 
 def bench_onnxslim(input, output):
     command = f"onnxslim {input} {output}"
     result = bench_main(command)
     return result
 
+
 def bench_onnxsim(input, output):
     command = f"onnxsim {input} {output}"
     result = bench_main(command)
     return result
-    
+
+
 def bench_polygraphy(input, output):
     command = f"polygraphy surgeon sanitize --fold-constants {input} -o {output}"
     result = bench_main(command)
     return result
+
 
 class TestModelZoo:
     def transform_and_check(self, name, filename, transformation_func, suffix, check_func):
@@ -55,13 +61,15 @@ class TestModelZoo:
             outputs = ort_sess.run(None, {"input": input_data, "sr": sr, "state": state})
             assert outputs is not None, "Inference failed on transformed model."
 
-        name = request.node.originalname[len("test_"):]
+        name = request.node.originalname[len("test_") :]
         filename = f"{MODELZOO_PATH}/{name}/{name}.onnx"
 
         summary_list = [summarize_model(filename)]
         summary_list.append(self.transform_and_check(name, filename, bench_onnxslim, "onnxslim", check_model_inference))
         summary_list.append(self.transform_and_check(name, filename, bench_onnxsim, "onnxsim", check_model_inference))
-        summary_list.append(self.transform_and_check(name, filename, bench_polygraphy, "polygraphy", check_model_inference))
+        summary_list.append(
+            self.transform_and_check(name, filename, bench_polygraphy, "polygraphy", check_model_inference)
+        )
 
         summary_list = [summary for summary in summary_list if summary is not None]
 

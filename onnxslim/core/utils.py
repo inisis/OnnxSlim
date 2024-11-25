dea803a23c8fe2953034af4d9c997f57f763e2a6
UltralyticsAssistant <web@ultralytics.com>
2024-11-25 17:44:34 +0000

    Auto-format by https://ultralytics.com/actions

--- onnxslim/core/utils.py
+++ onnxslim/core/utils.py
@@ -1,5 +1,5 @@
 from onnxslim.core.pattern import get_node_feeds, get_node_users
-from onnxslim.third_party.onnx_graphsurgeon.ir.tensor import Variable, Constant
+from onnxslim.third_party.onnx_graphsurgeon.ir.tensor import Constant, Variable
 
 
 def delete_node(node, input_var_idx=0, output_var_idx=0):

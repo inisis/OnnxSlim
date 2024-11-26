489c03977f4865c450af37c32f08d1a634e778fa
張小凡 <2672931+whyb@users.noreply.github.com>
2024-11-26 15:56:31 +0800

    Added onnxslim command test. input shapes/FP32 to FP16

--- tests/test_onnxslim.py
+++ tests/test_onnxslim.py
@@ -12,21 +12,38 @@ FILENAME = f"{MODELZOO_PATH}/resnet18/resnet18.onnx"
 
 
 class TestFunctional:
-    def test_basic(self, request):
-        """Test the basic functionality of the slim function."""
+    def __test_command_basic(self, request, in_model_path=FILENAME, out_model_name="resnet18.onnx", arg_str=""):
         with tempfile.TemporaryDirectory() as tempdir:
-            summary = summarize_model(slim(FILENAME), request.node.name)
+            summary = summarize_model(slim(in_model_path), request.node.name))
             print_model_info_as_table(summary)
-            output_name = os.path.join(tempdir, "resnet18.onnx")
-            slim(FILENAME, output_name)
-            slim(FILENAME, output_name, model_check=True)
-
-            command = f"onnxslim {FILENAME} {output_name}"
+            out_model_path = os.path.join(tempdir, out_model_name)
+            slim(in_model_path, out_model_path)
+            slim(in_model_path, out_model_path, model_check=True)
+            command = f"onnxslim {arg_str} \"{in_model_path}\" \"{out_model_path}\""
             result = subprocess.run(command, shell=True, capture_output=True, text=True)
             output = result.stderr.strip()
             # Assert the expected return code
             print(output)
             assert result.returncode == 0
+            return in_model_path, out_model_path
+
+    def test_basic(self, request):
+        """Test the basic functionality of the slim function."""
+        self.__test_command_basic(request)
+
+    def test_input_shape_modification(self, request):
+        """Test the modification of input shapes."""
+        input_shape_arg_str = "--input-shapes input:1,3,224,224"
+        self.__test_command_basic(request, FILENAME, "resnet18.onnx", input_shape_arg_str)
+
+    def test_fp322fp16_conversion(self, request):
+        """Test the conversion of an ONNX model from FP32 to FP16 precision."""
+        dtype_fp16_arg_str = "--dtype fp16"
+        _, out_model_path = self.__test_command_basic(
+            request, FILENAME, "resnet18_fp16.onnx", dtype_fp16_arg_str
+        )
+        dtype_fp32_arg_str = "--dtype fp32"
+        self.__test_command_basic(request, out_model_path, "resnet18_fp32.onnx", dtype_fp32_arg_str)
 
 
 class TestFeature:

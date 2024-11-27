64aee5fd507b096b8bf24353455458a618ac55be
UltralyticsAssistant <web@ultralytics.com>
2024-11-27 12:23:20 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_onnxslim.py
+++ tests/test_onnxslim.py
@@ -2,8 +2,9 @@ import os
 import subprocess
 import tempfile
 
-import pytest
 import numpy as np
+import pytest
+
 from onnxslim import slim
 from onnxslim.utils import summarize_model
 
@@ -13,15 +14,17 @@ FILENAME = f"{MODELZOO_PATH}/resnet18/resnet18.onnx"
 
 class TestFunctional:
     def run_basic_test(self, in_model_path, out_model_path, **kwargs):
-        check_func = kwargs.get('check_func', None)
-        kwargs_api = kwargs.get('api', {})
-        kwargs_bash = kwargs.get('bash', "")
+        check_func = kwargs.get("check_func", None)
+        kwargs_api = kwargs.get("api", {})
+        kwargs_bash = kwargs.get("bash", "")
         summary = summarize_model(slim(in_model_path, **kwargs_api), in_model_path)
-        if check_func: check_func(summary)
+        if check_func:
+            check_func(summary)
 
         slim(in_model_path, out_model_path, **kwargs_api)
         summary = summarize_model(out_model_path, out_model_path)
-        if check_func: check_func(summary)
+        if check_func:
+            check_func(summary)
 
         command = f'onnxslim "{in_model_path}" "{out_model_path}", {kwargs_bash}'
 
@@ -32,7 +35,8 @@ class TestFunctional:
         assert result.returncode == 0
 
         summary = summarize_model(out_model_path, out_model_path)
-        if check_func: check_func(summary)
+        if check_func:
+            check_func(summary)
 
     def test_basic(self, request):
         with tempfile.TemporaryDirectory() as tempdir:
@@ -46,9 +50,9 @@ class TestFunctional:
         with tempfile.TemporaryDirectory() as tempdir:
             out_model_path = os.path.join(tempdir, "resnet18.onnx")
             kwargs = {}
-            kwargs['bash'] = "--input-shapes input:1,3,224,224"
-            kwargs['api'] = {'input_shapes': ["input:1,3,224,224"]}
-            kwargs['check_func'] = check_func
+            kwargs["bash"] = "--input-shapes input:1,3,224,224"
+            kwargs["api"] = {"input_shapes": ["input:1,3,224,224"]}
+            kwargs["check_func"] = check_func
             self.run_basic_test(FILENAME, out_model_path, **kwargs)
 
     def test_input_modification(self, request):
@@ -59,9 +63,9 @@ class TestFunctional:
         with tempfile.TemporaryDirectory() as tempdir:
             out_model_path = os.path.join(tempdir, "resnet18.onnx")
             kwargs = {}
-            kwargs['bash'] = "--inputs /maxpool/MaxPool_output_0 /layer1/layer1.0/relu/Relu_output_0"
-            kwargs['api'] = {"inputs": ["/maxpool/MaxPool_output_0", "/layer1/layer1.0/relu/Relu_output_0"]}
-            kwargs['check_func'] = check_func
+            kwargs["bash"] = "--inputs /maxpool/MaxPool_output_0 /layer1/layer1.0/relu/Relu_output_0"
+            kwargs["api"] = {"inputs": ["/maxpool/MaxPool_output_0", "/layer1/layer1.0/relu/Relu_output_0"]}
+            kwargs["check_func"] = check_func
             self.run_basic_test(FILENAME, out_model_path, **kwargs)
 
     def test_output_modification(self, request):
@@ -71,9 +75,9 @@ class TestFunctional:
         with tempfile.TemporaryDirectory() as tempdir:
             out_model_path = os.path.join(tempdir, "resnet18.onnx")
             kwargs = {}
-            kwargs['bash'] = "--outputs /Flatten_output_0"
-            kwargs['api'] = {"outputs": ["/Flatten_output_0"]}
-            kwargs['check_func'] = check_func
+            kwargs["bash"] = "--outputs /Flatten_output_0"
+            kwargs["api"] = {"outputs": ["/Flatten_output_0"]}
+            kwargs["check_func"] = check_func
             self.run_basic_test(FILENAME, out_model_path, **kwargs)
 
     def test_dtype_conversion(self, request):
@@ -86,24 +90,24 @@ class TestFunctional:
         with tempfile.TemporaryDirectory() as tempdir:
             out_fp16_model_path = os.path.join(tempdir, "resnet18_fp16.onnx")
             kwargs = {}
-            kwargs['bash'] = "--dtype fp16"
-            kwargs['api'] = {"dtype": "fp16"}
-            kwargs['check_func'] = check_func_fp16
+            kwargs["bash"] = "--dtype fp16"
+            kwargs["api"] = {"dtype": "fp16"}
+            kwargs["check_func"] = check_func_fp16
             self.run_basic_test(FILENAME, out_fp16_model_path, **kwargs)
 
             out_fp32_model_path = os.path.join(tempdir, "resnet18_fp32.onnx")
             kwargs = {}
-            kwargs['bash'] = "--dtype fp32"
-            kwargs['api'] = {"dtype": "fp32"}
-            kwargs['check_func'] = check_func_fp32
+            kwargs["bash"] = "--dtype fp32"
+            kwargs["api"] = {"dtype": "fp32"}
+            kwargs["check_func"] = check_func_fp32
             self.run_basic_test(out_fp16_model_path, out_fp32_model_path, **kwargs)
 
     def test_save_as_external_data(self, request):
         with tempfile.TemporaryDirectory() as tempdir:
             out_model_path = os.path.join(tempdir, "resnet18.onnx")
             kwargs = {}
-            kwargs['bash'] = "--save-as-external-data"
-            kwargs['api'] = {"save_as_external_data": True}
+            kwargs["bash"] = "--save-as-external-data"
+            kwargs["api"] = {"save_as_external_data": True}
             self.run_basic_test(FILENAME, out_model_path, **kwargs)
             assert os.path.getsize(out_model_path) < 1e5
 

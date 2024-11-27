b7f4605c3b44bf78eea93c5410d21716956f4f18
張小凡 <2672931+whyb@users.noreply.github.com>
2024-11-27 10:39:07 +0800

    The arg_str should be placed after input_model and output_model

--- tests/test_onnxslim.py
+++ tests/test_onnxslim.py
@@ -19,7 +19,7 @@ class TestFunctional:
             out_model_path = os.path.join(tempdir, out_model_name)
             slim(in_model_path, out_model_path)
             slim(in_model_path, out_model_path, model_check=True)
-            command = f'onnxslim {arg_str} "{in_model_path}" "{out_model_path}"'
+            command = f'onnxslim "{in_model_path}" "{out_model_path}" {arg_str}'
             result = subprocess.run(command, shell=True, capture_output=True, text=True)
             output = result.stderr.strip()
             # Assert the expected return code

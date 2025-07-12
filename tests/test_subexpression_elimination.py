3fb4c2a51948d2836b473ccfd97083ad2655df6e
UltralyticsAssistant <web@ultralytics.com>
2025-07-12 11:44:19 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_subexpression_elimination.py
+++ tests/test_subexpression_elimination.py
@@ -1,17 +1,14 @@
 import os
 
-import numpy as np
 import onnx
-import pytest
 import torch
 import torch.nn as nn
 
 import onnxslim.third_party.onnx_graphsurgeon as gs
 from onnxslim.core.optimization.subexpression_elimination import (
-    subexpression_elimination,
-    find_and_remove_replaceable_nodes,
     can_be_replaced,
-    sequences_equal
+    sequences_equal,
+    subexpression_elimination,
 )
 from onnxslim.core.optimization.weight_tying import tie_weights
 
@@ -27,7 +24,7 @@ class TestSubexpressionElimination:
                 self.linear2 = nn.Linear(10, 20, bias=False)
                 # Both linear layers will have the same weights
                 self.linear2.weight = nn.Parameter(self.linear1.weight.detach().clone())
-            
+
             def forward(self, x):
                 # Apply the same operation twice on the same input
                 y1 = self.linear1(x)
@@ -45,8 +42,8 @@ class TestSubexpressionElimination:
 
         # Import graph and apply subexpression_elimination
         graph = gs.import_onnx(onnx.load(filename))
-        graph.fold_constants().cleanup().toposort()      
-        tie_weights(graph)          
+        graph.fold_constants().cleanup().toposort()
+        tie_weights(graph)
         graph.cleanup().toposort()
         initial_node_count = len(graph.nodes)
         subexpression_elimination(graph)
@@ -61,54 +58,54 @@ class TestSubexpressionElimination:
         # Test with equal sequences
         seq1 = [1, 2, 3]
         seq2 = [1, 2, 3]
-        assert sequences_equal(seq1, seq2) == True
+        assert sequences_equal(seq1, seq2)
 
         # Test with different length sequences
         seq3 = [1, 2, 3, 4]
-        assert sequences_equal(seq1, seq3) == False
+        assert not sequences_equal(seq1, seq3)
 
         # Test with same length but different elements
         seq4 = [1, 2, 4]
-        assert sequences_equal(seq1, seq4) == False
+        assert not sequences_equal(seq1, seq4)
 
         # Test with empty sequences
-        assert sequences_equal([], []) == True
+        assert sequences_equal([], [])
 
     def test_can_be_replaced_function(self):
         """Test the can_be_replaced function with mock nodes."""
         # Create mock nodes with the same attributes and inputs
         node1 = gs.Node(op="Add", name="add1")
         node1.attrs = {"axis": 1}
-        
+
         node2 = gs.Node(op="Add", name="add2")
         node2.attrs = {"axis": 1}
-        
+
         # Create mock inputs
         input1 = gs.Variable(name="input1")
         input2 = gs.Variable(name="input2")
-        
+
         # Set same inputs for both nodes
         node1.inputs = [input1, input2]
         node2.inputs = [input1, input2]
-        
+
         # Nodes should be replaceable
-        assert can_be_replaced(node1, node2) == True
-        
+        assert can_be_replaced(node1, node2)
+
         # Test with different operations
         node3 = gs.Node(op="Mul", name="mul1")
         node3.attrs = {"axis": 1}
         node3.inputs = [input1, input2]
-        assert can_be_replaced(node1, node3) == False
-        
+        assert not can_be_replaced(node1, node3)
+
         # Test with different attributes
         node4 = gs.Node(op="Add", name="add3")
         node4.attrs = {"axis": 2}  # Different axis
         node4.inputs = [input1, input2]
-        assert can_be_replaced(node1, node4) == False
-        
+        assert not can_be_replaced(node1, node4)
+
         # Test with different inputs
         input3 = gs.Variable(name="input3")
         node5 = gs.Node(op="Add", name="add4")
         node5.attrs = {"axis": 1}
         node5.inputs = [input1, input3]  # Different second input
-        assert can_be_replaced(node1, node5) == False 
+        assert not can_be_replaced(node1, node5)

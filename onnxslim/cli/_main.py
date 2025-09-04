a413233ff6543485712b8ae0f11e112810609f7b
RizwanMunawar <muhammadrizwanmunawar123@gmail.com>
2025-09-04 11:46:48 +0500

    remove future import

--- onnxslim/cli/_main.py
+++ onnxslim/cli/_main.py
@@ -1,9 +1,7 @@
-from __future__ import annotations
-
 import onnx
 
 
-def slim(model: str | onnx.ModelProto | list[str | onnx.ModelProto], *args, **kwargs):
+def slim(model: Union[str, onnx.ModelProto, list[Union[str, onnx.ModelProto]]], *args, **kwargs):
     import os
     import time
     from pathlib import Path
